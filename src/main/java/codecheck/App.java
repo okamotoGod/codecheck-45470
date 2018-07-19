c7aa3665af06f664415f8919ad24a4c488f61412
Okamoto Daisuke <okamoto_d@worksap.co.jp>
2018-07-19 10:13:35 +0900

    waefasda sdfas f

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,6 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
-		}
+		System.out.println("Hello "+ args[0] + "!");
 	}
 }
